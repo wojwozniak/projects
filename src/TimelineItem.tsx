f3a94289d7a87827d316d3b884d97664aaefd18c
Wojtek <pulchnymamut@gmail.com>
2023-12-26 19:44:01 +0100

    add link to project

--- src/TimelineItem.tsx
+++ src/TimelineItem.tsx
@@ -3,13 +3,21 @@ import { FC } from 'react';
 interface TimelineItemProps {
   projectName: string;
   description: string;
+  link?: string;
 }
 
-const TimelineItem: FC<TimelineItemProps> = ({ projectName, description }) => {
+const TimelineItem: FC<TimelineItemProps> = ({ projectName, description, link }) => {
   return (
     <div className="border-l-4 border-gold pl-4 mb-4">
       <h3 className="text-2xl font-medium">{projectName}</h3>
       <p className="text-lg">{description}</p>
+      {link &&
+        <a href={link}
+          target="_blank"
+          className="text-gold hover:underline">
+          Link
+        </a>
+      }
     </div>
   );
 }
