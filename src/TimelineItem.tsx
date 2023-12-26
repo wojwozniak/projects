367703ad29e358136aa88582982febb718b021a8
Wojtek <pulchnymamut@gmail.com>
2023-12-26 19:45:26 +0100

    Update TimelineItem.tsx

--- src/TimelineItem.tsx
+++ src/TimelineItem.tsx
@@ -4,15 +4,23 @@ interface TimelineItemProps {
   projectName: string;
   description: string;
   link?: string;
+  secondLink?: string;
 }
 
-const TimelineItem: FC<TimelineItemProps> = ({ projectName, description, link }) => {
+const TimelineItem: FC<TimelineItemProps> = ({ projectName, description, link, secondLink }) => {
   return (
     <div className="border-l-4 border-gold pl-4 mb-4">
       <h3 className="text-2xl font-medium">{projectName}</h3>
       <p className="text-lg">{description}</p>
       {link &&
         <a href={link}
+          target="_blank"
+          className="text-gold hover:underline">
+          Link
+        </a>
+      }
+      {secondLink &&
+        <a href={secondLink}
           target="_blank"
           className="text-gold hover:underline">
           Link
