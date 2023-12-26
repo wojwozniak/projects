0fda30c40126ede005fc3aa59277fa633b2bbf50
Wojtek <pulchnymamut@gmail.com>
2023-12-26 19:30:57 +0100

    tailwind setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 export default {
   content: ['./index.html', './src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        gold: '#D4AF37',
+      },
+    },
   },
   plugins: [],
 }
