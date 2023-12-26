5f1a75bd416996e8f32c62b844cdfbe967598b64
Wojtek <pulchnymamut@gmail.com>
2023-12-26 19:35:53 +0100

    fix declaration

--- src/TimelineItem.tsx
+++ src/TimelineItem.tsx
@@ -1,5 +1,11 @@
+import { FC } from 'react';
 
-const TimelineItem = ({ projectName, description }: { projectName: string, description: string }) => {
+interface TimelineItemProps {
+  projectName: string;
+  description: string;
+}
+
+const TimelineItem: FC<TimelineItemProps> = ({ projectName, description }) => {
   return (
     <div className="border-l-4 border-gold pl-4 mb-4">
       <h3 className="text-2xl font-medium">{projectName}</h3>
