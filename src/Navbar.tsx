b89a1e63eb57f9104078037dc8368c2c44085afb
Wojtek <pulchnymamut@gmail.com>
2024-01-02 00:20:36 +0100

    fix navbar

--- src/Navbar.tsx
+++ src/Navbar.tsx
@@ -6,18 +6,20 @@ const Navbar: React.FC = () => {
   const { theme, toggleTheme } = useTheme();
 
   return (
-    <nav className="p-4 transition-colors duration-300 ease-in-out flex justify-between items-center"
-      style={{ borderBottom: `1px solid var(--color-border)`, backgroundColor: 'var(--color-background)', color: 'var(--color-text)' }}>
-      <h1>wojwozniak | My projects</h1>
-      <div>
-        <span className="mr-2">
-          {theme === 'dark' ? 'Dark mode on' : 'Dark mode off'}
-        </span>
-        <Switch checked={theme === 'dark'}
-          onChange={toggleTheme} />
-      </div>
-
-    </nav>
+    <>
+      <nav className="fixed top-0 left-0 right-0 p-4 transition-colors duration-300 ease-in-out flex justify-between items-center"
+        style={{ borderBottom: `1px solid var(--color-border)`, backgroundColor: 'var(--color-background)', color: 'var(--color-text)', zIndex: 1000 }}>
+        <h1 style={{ lineHeight: '1.5em' }}>wojwozniak | My projects</h1>
+        <div style={{ display: 'flex', alignItems: 'center', lineHeight: '1.5em' }}>
+          <span className="mr-2">
+            {theme === 'dark' ? 'Dark mode on' : 'Dark mode off'}
+          </span>
+          <Switch checked={theme === 'dark'}
+            onChange={toggleTheme} />
+        </div>
+      </nav>
+      <div style={{ paddingTop: '68px' }} />
+    </>
   );
 };
 
