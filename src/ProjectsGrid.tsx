e5c2c5d596d3284179d0590b286e4c9fee987c8b
Wojtek <pulchnymamut@gmail.com>
2024-01-01 23:45:01 +0100

    project 2

--- src/ProjectsGrid.tsx
+++ src/ProjectsGrid.tsx
@@ -1,10 +1,27 @@
 import ProjectComponent from './ui/ProjectComponent'
 import img__pustynna from './../assets/images/pustynna.png'
+import img__eCommerce from './../assets/images/nodeshop.png'
 import { FaExternalLinkAlt, FaGithub } from "react-icons/fa";
 
 const ProjectsGrid = () => {
   return (
     <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 p-4">
+      <ProjectComponent
+        title="E-commerce website (backend)"
+        date="December 2023"
+        techStack="Node.js, Express, MongoDB, Mongoose, EJS, Tailwind"
+        screenshotUrl={img__eCommerce}
+        description={
+          <div>
+            <p>
+              E-commerce website (built exclusively with backend technologies). Created with Node.js, used MongoDB database. It has a fully functional shopping cart, user authentication, admin panel and more.
+            </p>
+            <p>
+              User authentication is done with custom middleware (and password hashing with bcrypt). Admin panel is protected with a custom middleware as well. The website is styled with Tailwind CSS. It uses EJS as a templating engine.
+            </p>
+          </div>
+        }
+      />
       <ProjectComponent
         title="Website for a scout event"
         date="March - May 2022"
