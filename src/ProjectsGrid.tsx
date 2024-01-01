23c70ed61e42b9f72ffea59353da70542f7699dd
Wojtek <pulchnymamut@gmail.com>
2024-01-02 00:08:48 +0100

    project 4

--- src/ProjectsGrid.tsx
+++ src/ProjectsGrid.tsx
@@ -2,6 +2,7 @@ import ProjectComponent from './ui/ProjectComponent'
 import img__pustynna from './../assets/images/pustynna.png'
 import img__eCommerce from './../assets/images/nodeshop.png'
 import img__bezier from './../assets/images/bezier.png'
+import img__d3charts from './../assets/images/d3charts.png'
 import { FaExternalLinkAlt, FaGithub } from "react-icons/fa";
 
 const ProjectsGrid = () => {
@@ -64,6 +65,52 @@ const ProjectsGrid = () => {
                   Github repository
                 </a>
               </li>
+            </ul>
+          </div>
+        }
+      />
+      <ProjectComponent
+        title="Collection of D3.js charts"
+        date="June 2022"
+        techStack="D3.js, Javascript, Vue.js"
+        screenshotUrl={img__d3charts}
+        description={
+          <div>
+            <p>
+              I have created a collection of charts to learn D3.js library. Data for charts is from FreeCodeCamp - I have finished their Data Visualization course and did those charts as a final project to get a certificate.
+            </p>
+            <p>
+              Few months later I decided to rewrite them using Vue.js. I have also made them into a single website for easier navigation.
+            </p>
+            <p>
+              Website is optimised for desktop devices - a lot of information is displayed on hover.
+            </p>
+            <ul className='flex flex-row justify-between w-full px-4 pt-4'>
+              <li className='flex flex-row items-center justify-center gap-2'>
+                <FaExternalLinkAlt className='text-blue-500 hover:underline' />
+                <a href="https://wojwozniak.com/d3-charts/index.html"
+                  className="text-blue-500 hover:underline"
+                  target='_blank'>
+                  Live website
+                </a>
+              </li>
+              <li className='flex flex-row items-center justify-center gap-2'>
+                <FaGithub className='text-blue-500 hover:underline' />
+                <a href="https://github.com/wojwozniak/d3-charts"
+                  className="text-blue-500 hover:underline"
+                  target='_blank'>
+                  Github repository
+                </a>
+              </li>
+              <li className='flex flex-row items-center justify-center gap-2'>
+                <FaExternalLinkAlt className='text-blue-500 hover:underline' />
+                <a href="https://www.freecodecamp.org/certification/wojwozniak/data-visualization"
+                  className="text-blue-500 hover:underline"
+                  target='_blank'>
+                  FCC Certificate
+                </a>
+              </li>
+              
             </ul>
           </div>
         }
