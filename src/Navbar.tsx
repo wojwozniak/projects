9bb47dfd43585817c98ba51c60cf703d9c28ef4a
Wojtek <pulchnymamut@gmail.com>
2024-01-02 01:20:10 +0100

    add link in navbar

--- src/Navbar.tsx
+++ src/Navbar.tsx
@@ -9,7 +9,9 @@ const Navbar: React.FC = () => {
     <>
       <nav className="fixed top-0 left-0 right-0 p-4 transition-colors duration-300 ease-in-out flex justify-between items-center"
         style={{ borderBottom: `1px solid var(--color-border)`, backgroundColor: 'var(--color-background)', color: 'var(--color-text)', zIndex: 1000 }}>
-        <h1 style={{ lineHeight: '1.5em' }}>wojwozniak | My projects</h1>
+        <h1 style={{ lineHeight: '1.5em' }}>
+          <a href="http://wojwozniak.com/" style={{ color: 'inherit', textDecoration: 'none', cursor: 'pointer' }}>wojwozniak</a> | My projects
+        </h1>
         <div style={{ display: 'flex', alignItems: 'center', lineHeight: '1.5em' }}>
           <span className="mr-2">
             Dark mode
